69b43adf20ded01c75ce6273e2c6f5235540ceb1
SGYZ0910 <15837994614@163.com>
2019-10-10 00:40:21 +0800

    Site updated: 2019-10-10 00:40:16

--- box/games/kuang/firebase.js
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1>Not Found</h1>
-<p>The requested URL /4399swf/upload_swf/ftp21/ssj/20170329/j7/firebase.js was not found on this server.</p>
-</body></html>
